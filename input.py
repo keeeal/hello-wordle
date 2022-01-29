0f4094bc725356e816107481a3d565fe6b0cc36f
swculley <swculley@gmail.com>
2022-01-29 20:53:45 +1030

    reads files

---
+++ input.py
@@ -0,0 +1,18 @@
+def read_files(f1: str, f2: str) -> tuple[list[str], list[str]]:
+
+    with open(f1) as f:
+        guesses = f.readlines()
+
+    with open(f2) as f:
+        answers = f.readlines()
+
+    guesses = list(map(str.strip, guesses))
+    answers = list(map(str.strip, answers))
+
+    return guesses, answers
+
+
+if __name__ == "__main__":
+    x, y = read_files("wordle-allowed-guesses.txt", "wordle-answers-alphabetical.txt")
+    print(x[:10])
+    print(y[:10])
