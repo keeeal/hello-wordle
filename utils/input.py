296e2a07689d4505c8174aaa1658671bbf496b22
swculley <swculley@gmail.com>
2022-02-01 08:52:17 +1030

    asked property of answers in input

--- utils/input.py
+++ utils/input.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 def read_files(f1: str, f2: str) -> tuple[list[str], list[str]]:
 
     with open(f1) as f:
@@ -13,6 +15,23 @@ def read_files(f1: str, f2: str) -> tuple[list[str], list[str]]:
 
 
 if __name__ == "__main__":
-    x, y = read_files("wordle-allowed-guesses.txt", "wordle-answers-alphabetical.txt")
+    x, y = read_files(Path("data") / "wordle-allowed-guesses.txt", Path("data") / "wordle-answers-alphabetical.txt")
     print(x[:10])
     print(y[:10])
+
+    print("words with repeated letter")
+    bigcount = 0
+    for word in y:
+        count = {}
+        for s in word:
+            if s in count:
+                count[s] += 1
+                bigcount +=1
+                print(word)
+                break
+            else:
+                count[s] = 1
+    
+    print(bigcount)
+    print(bigcount/len(y))
+    
