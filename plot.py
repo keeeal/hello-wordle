b3c2dd4bd2b027ca992d015abfaab54ffda5a362
jkeal <jkeal@micro-x.com>
2022-02-01 23:34:26 +1030

    plot multiple log files

--- plot.py
+++ plot.py
@@ -1,40 +1,48 @@
+from functools import partial
 import json
+from itertools import chain
 from pathlib import Path
 from typing import Iterator
 
-from pandas import DataFrame
+from pandas import DataFrame, concat
 from seaborn import lineplot
 
 
-def read_log(file: Path) -> Iterator:
+def read_log(file: Path, bin: bool = False) -> DataFrame:
     with open(file) as f:
-        return map(json.loads, f.readlines())
+        data = DataFrame(map(json.loads, f.readlines()))
 
+    if bin:
+        binned_data = []
 
-def plot(file: str) -> None:
-    data = DataFrame(read_log(file))
-    binned_data = []
-
-    for player in set(data["player"]):
-        for vocabulary in set(data["vocabulary"]):
-            game_lengths = list(
-                data.loc[
-                    (data["player"] == player) & (data["vocabulary"] == vocabulary),
-                    "game_length",
-                ]
-            )
-            for game_length in set(game_lengths):
-                binned_data.append(
-                    dict(
-                        player=player,
-                        vocabulary=vocabulary,
-                        game_length=game_length,
-                        count=game_lengths.count(game_length),
-                    )
+        for player in set(data["player"]):
+            for vocabulary in set(data["vocabulary"]):
+                game_lengths = list(
+                    data.loc[
+                        (data["player"] == player) & (data["vocabulary"] == vocabulary),
+                        "game_length",
+                    ]
                 )
+                for game_length in set(game_lengths):
+                    binned_data.append(
+                        dict(
+                            player=player,
+                            vocabulary=vocabulary,
+                            game_length=game_length,
+                            count=game_lengths.count(game_length),
+                        )
+                    )
+
+        data = DataFrame(binned_data)
+
+    return data
+
+
+def plot(files: list[Path]) -> None:
+    data = concat(map(partial(read_log, bin=True), files), ignore_index=True)
 
     ax = lineplot(
-        data=DataFrame(binned_data),
+        data=data,
         x="game_length",
         y="count",
         hue="player",
@@ -47,5 +55,5 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", type=Path)
+    parser.add_argument("files", nargs="+", type=Path)
     plot(**vars(parser.parse_args()))
